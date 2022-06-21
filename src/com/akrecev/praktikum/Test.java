83b03b273f139015cf46bcc84846c2cd7f65abe3
akrecev <akrecev@gmail.com>
2022-06-21 21:25:14 +0300

    new

--- src/com/akrecev/praktikum/Test.java
+++ src/com/akrecev/praktikum/Test.java
@@ -2,21 +2,15 @@ package com.akrecev.praktikum;
 
 public class Test {
     public static void main(String[] args) {
-        double[] expenses = {1772.5, 367.0, 120.6, 2150.2, 874.0, 1.0, 1459.4};
-        double maxExpense = findMaxExpense(expenses); // Вызовите метод и присвойте maxExpense значение его результата
-        System.out.println("Самая большая трата недели " + maxExpense);
+        String catName = "Пиксель";
+        String hamsterName = "Байт";
+        sayHello("Андрей");
+        sayHello(catName);
+        sayHello(hamsterName);
+
     }
 
-    // Объявите метод findMaxExpense
-    public static double findMaxExpense(double[] expenses) {
-        // Реализуйте метод. Чтобы найти самую большую трату, воспользуйтесь циклом
-        double maxExpense = expenses[0];
-        for (double expense : expenses) {
-            if (expense > maxExpense) {
-                maxExpense = expense;
-            }
-        }
-        return maxExpense;
-
+    public static void sayHello(String username) {
+        System.out.println("Привет, " + username);
     }
 }
