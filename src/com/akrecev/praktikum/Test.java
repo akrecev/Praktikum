ce6108fe6f95c6e3d2573bda0c07eeadd0d479be
akrecev <akrecev@gmail.com>
2022-06-21 21:34:44 +0300

    new

--- src/com/akrecev/praktikum/Test.java
+++ src/com/akrecev/praktikum/Test.java
@@ -1,16 +1,54 @@
 package com.akrecev.praktikum;
 
+import java.util.Scanner;
+
 public class Test {
     public static void main(String[] args) {
-        String catName = "Пиксель";
-        String hamsterName = "Байт";
-        sayHello("Андрей");
-        sayHello(catName);
-        sayHello(hamsterName);
+        Scanner scanner = new Scanner(System.in);
+        System.out.println("Какую зарплату вы получите в этом месяце?");
+        int salary = scanner.nextInt();
+        System.out.println("Сколько планируете потратить на транспорт?");
+        int transportMoney = scanner.nextInt();
+        System.out.println("Сколько запланировано на супермаркеты?");
+        int foodMoney = scanner.nextInt();
+        System.out.println("Какую сумму хотите отложить?");
+        int savings = scanner.nextInt();
 
+        // Вызовите метод correctExpenses с правильными аргументами
+        correctExpenses(salary, transportMoney, foodMoney, savings);
+
+        // Напечатайте запланированные траты
+        System.out.println("Вы планировали потратить: транспорт — " + transportMoney + ", "
+                + "еда — " + foodMoney + ", "
+                + "сбережения — " + savings + ".");
     }
 
-    public static void sayHello(String username) {
-        System.out.println("Привет, " + username);
+    // Объявите метод correctExpenses
+    public static void correctExpenses(int salary, int transportMoney, int foodMoney, int savings) {
+        // Тело метода дано ниже
+
+        int expensesSum = transportMoney + foodMoney + savings; // Считаем расходы
+        if (expensesSum > salary) { // Проверяем, не превышают ли расходы зарплату
+            int lackMoney = expensesSum - salary; // Считаем, сколько не хватает
+
+            // Пока не начнёт хватать денег на транспорт — сокращаем траты на 100 рублей
+            while ((salary - foodMoney) < transportMoney) {
+                foodMoney = foodMoney - 100;
+            }
+            // Если не хватает денег на жизнь — не откладываем
+            if (transportMoney + foodMoney + savings > salary) {
+                savings = 0;
+            }
+
+            // Печатаем рекомендации
+            System.out.println("Придётся пересмотреть планы, вам не хватает " + lackMoney);
+            System.out.println("Рекомендуемые траты: "
+                    + "еда — " + foodMoney + ", "
+                    + "сбережения — " + savings + ".");
+        } else {
+            int leftMoney = salary - expensesSum; // Считаем излишек средств
+            System.out.println("В этом месяце дебет с кредитом сошлись!");
+            System.out.println("Свободных средств " + leftMoney);
+        }
     }
 }
